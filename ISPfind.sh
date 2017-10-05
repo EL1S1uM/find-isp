6e42ba29cfeb9845a639c7c649126968191f9306
Felipe <felipe.citnnet@gmail.com>
2017-10-05 03:57:12 -0300

    Create ISPfind.sh

---
+++ ISPfind.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#EL1S1uM Was Here
+
+if [ $# != 1 ]
+then
+        echo "Uso: $0 ips.txt"
+else
+
+for lista in $(cat $1)
+do
+        isp=$(curl -s ipinfo.io/$lista | grep "org" | cut -d '"' -f 4)
+        echo $lista" -" $isp
+done
+fi
