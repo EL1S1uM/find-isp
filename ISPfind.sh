9ffa6f03a645f5c4c4d201190fb128f33de966c3
Felipe <felipe.citnnet@gmail.com>
2017-10-21 12:18:11 -0200

    Update ISPfind.sh

--- ISPfind.sh
+++ ISPfind.sh
@@ -2,13 +2,11 @@
 
 #EL1S1uM Was Here
 
-if [ $# != 1 ]
-then
+if [ $# != 1 ]; then
         echo "Uso: $0 ips.txt"
 else
 
-for lista in $(cat $1)
-do
+for lista in $(cat $1); do
         isp=$(curl -s ipinfo.io/$lista | grep "org" | cut -d '"' -f 4)
         echo $lista" -" $isp
 done
